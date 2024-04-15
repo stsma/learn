dfa13632b94da83c2232ebecec160a98e56c55be
smakilan <stephensmakilan@gmail.com>
2024-04-16 05:29:35 +0800

    https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/

--- Program.cs
+++ Program.cs
@@ -1,1 +1,116 @@
-﻿
+﻿using test;
+
+Coffee cup = cc.PourCoffee();
+Console.WriteLine("coffee is ready");
+
+var eggsTask = cc.FryEggsAsync(2);
+var baconTask = cc.FryBaconAsync(3);
+var toastTask = cc.MakeToastWithButterAndJamAsync(2);
+
+var breakfastTasks = new List<Task> { eggsTask, baconTask, toastTask };
+while (breakfastTasks.Count > 0)
+{
+    Task finishedTask = await Task.WhenAny(breakfastTasks);
+    if (finishedTask == eggsTask)
+    {
+        Console.WriteLine("eggs are ready");
+    }
+    else if (finishedTask == baconTask)
+    {
+        Console.WriteLine("bacon is ready");
+    }
+    else if (finishedTask == toastTask)
+    {
+        Console.WriteLine("toast is ready");
+    }
+    await finishedTask;
+    breakfastTasks.Remove(finishedTask);
+}
+
+Juice oj = cc.PourOJ();
+Console.WriteLine("oj is ready");
+Console.WriteLine("Breakfast is ready!");
+
+// These classes are intentionally empty for the purpose of this example. They are simply marker classes for the purpose of demonstration, contain no properties, and serve no other purpose.
+internal class Bacon { }
+internal class Coffee { }
+internal class Egg { }
+internal class Juice { }
+internal class Toast { }
+
+
+namespace test
+{
+    public static class cc
+    {
+
+
+        internal static async Task<Toast> MakeToastWithButterAndJamAsync(int number)
+        {
+            var toast = await ToastBreadAsync(number);
+            ApplyButter(toast);
+            ApplyJam(toast);
+
+            return toast;
+        }
+
+        internal static Juice PourOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        internal static void ApplyJam(Toast toast) =>
+            Console.WriteLine("Putting jam on the toast");
+
+        internal static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on the toast");
+
+        internal static async Task<Toast> ToastBreadAsync(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+            Console.WriteLine("Start toasting...");
+            await Task.Delay(3000);
+            Console.WriteLine("Remove toast from toaster");
+
+            return new Toast();
+        }
+
+        internal static async Task<Bacon> FryBaconAsync(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+            Console.WriteLine("cooking first side of bacon...");
+            await Task.Delay(3000);
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+            Console.WriteLine("cooking the second side of bacon...");
+            await Task.Delay(3000);
+            Console.WriteLine("Put bacon on plate");
+
+            return new Bacon();
+        }
+
+        internal static async Task<Egg> FryEggsAsync(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            await Task.Delay(3000);
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            await Task.Delay(3000);
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
+        internal static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
+        }
+    }
+}
